e5295d20de225b0fb60e3f992abacfeaa10d04ec
dennisHelavani <dengelovani@gmail.com>
2024-06-05 14:19:14 +0200

    faq section first option

--- script.js
+++ script.js
@@ -33,3 +33,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     soundButton.addEventListener('click', enableSound);
 });
+
+
+// FAQ Section and Its answers
+const answers = [
+    "Make sure to pack lightweight clothing, sunscreen, a hat, and plenty of water.",
+    "The typical tour lasts between 2 to 4 hours, depending on the package you choose.",
+    "The best time to visit is from March to May and September to November, when the weather is mild.",
+    "Yes, all meals are provided during the tour, including traditional Bedouin dishes.",
+    "Yes, the tour is suitable for children. We offer family-friendly packages."
+];
+
+function showAnswer(index) {
+    document.getElementById('faq-answer').textContent = answers[index - 1];
+}
+// Pulse animation below
+document.querySelectorAll('.faq-item').forEach(item => {
+    let isPulsing = false; // Initialize a flag to track if the animation is active
+  
+    item.addEventListener('mouseenter', () => {
+      if (!isPulsing) { // Check if the animation is not already active
+        isPulsing = true;
+        item.classList.add('pulsing');
+      }
+    });
+  
+    item.addEventListener('animationiteration', () => {
+      if (isPulsing) { // Check if the animation is active
+        isPulsing = false;
+        item.classList.remove('pulsing');
+      }
+    });
+  
+    item.addEventListener('mouseleave', () => {
+      if (isPulsing) { // Check if the animation is active
+        isPulsing = false;
+        item.classList.remove('pulsing');
+      }
+    });
+  });
+  
+  
